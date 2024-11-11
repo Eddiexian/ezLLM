835e15bb7d34908ab2c4debc92f6da7d2d318334
Eddiexian <43458446+Eddiexian@users.noreply.github.com>
2024-11-11 16:48:11 +0800

    Create router_index.ts

---
+++ router_index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/DragDreop.vue')
+    },
+    {
+      path: '/Edit',
+      name: 'Edit',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/EditDashBoard.vue')
+    },
+
+    {
+      path: '/CellMFG',
+      name: 'CellMFG',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CellMFGView.vue')
+    }
+  ]
+})
+
+export default router
